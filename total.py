6df07cb8dc6562ad039b721ba5f33736f6f7f4f6
varundey <varun.silversurfer@gmail.com>
2015-07-07 20:00:36 +0530

    total archive.org

--- total.py
+++ total.py
@@ -1,17 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 #file = open('total.txt','a')
-for main_page in range(1,13284):
+main_page=1
+while True:
 	print main_page
 	website="https://archive.org/details/image?&sort=-downloads&page=%d"%main_page
-	soup=BeautifulSoup(requests.get(website).content)
-	channeldiv=soup.findAll("div",{"class":"collection-title C C2"})
-	for div in channeldiv:
-		for channel_page in range(1,10):
-			channel= "https://archive.org"+div.find('a').get('href')+"?&sort=-downloads&page=%d"%channel_page
-			print channel
-			channel_link= BeautifulSoup(requests.get(channel).content)
-			channel_link=channel_link.findAll("img",{"class":"item-img"})
-			for img in channel_link:
-				img= img.get('source')
-				print "https://archive.org/services/img/"+img[14:]
+	website=BeautifulSoup(requests.get(website).content)
+	channeldiv=website.findAll("div",{"class":"collection-title C C2"})
+	if type(channeldiv) is list:
+		break
+	else:
+		for per_div in channeldiv:
+			channel_page=1
+			print per_div.find('a').find('div').string
+			while True:
+				print channel_page
+				channel_link= "https://archive.org"+per_div.find('a').get('href')+"?&sort=-downloads&page=%d"%channel_page
+				channel_content= BeautifulSoup(requests.get(channel_link).content)
+				channel_img=channel_content.findAll("img",{"class":"item-img"})
+				if type(channel_img) is list:
+					break
+				else:
+					for img in channel_img:
+						img= img.get('source')
+						print "https://archive.org/services/img/"+img[14:]
+						
+				channel_page+=1				
+	main_page+=1
