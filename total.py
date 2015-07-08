f9440de6e61f0b98d294ff24032a3c4255ca3bf8
Varun Dey <varun.silversurfer@gmail.com>
2015-07-08 12:51:08 +0530

    Writes channel name and links inside it to file

--- total.py
+++ total.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
-#file = open('total.txt','a')
+file = open('total.txt','a')
 main_page=1
 while True:
 	print main_page
+	file.write(str(main_page)+'\n')
 	website="https://archive.org/details/image?&sort=-downloads&page=%d"%main_page
 	website=BeautifulSoup(requests.get(website).content)
 	channeldiv=website.findAll("div",{"class":"collection-title C C2"})
@@ -13,8 +14,10 @@ while True:
 		for per_div in channeldiv:
 			channel_page=1
 			print per_div.find('a').find('div').string
+			file.write(str(per_div.find('a').find('div').string)+'\n')
 			while True:
 				print channel_page
+				file.write(str(channel_page)+'\n')
 				channel_link= "https://archive.org"+per_div.find('a').get('href')+"?&sort=-downloads&page=%d"%channel_page
 				channel_content= BeautifulSoup(requests.get(channel_link).content)
 				channel_img=channel_content.findAll("img",{"class":"item-img"})
@@ -23,7 +26,9 @@ while True:
 				else:
 					for img in channel_img:
 						img= img.get('source')
-						print "https://archive.org/services/img/"+img[14:]
+						m= "https://archive.org/services/img/"+img[14:]
+						print m
+						file.write(str(m)+'\n')
 						
 				channel_page+=1				
 	main_page+=1
