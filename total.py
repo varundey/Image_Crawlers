07eb2870482bed54c8641675f346081a03cb9ad3
varundey <varun.silversurfer@gmail.com>
2015-07-08 19:40:32 +0530

    all channels with image link and description inside channel

--- total.py
+++ total.py
@@ -3,7 +3,6 @@ import requests
 file = open('total.txt','a')
 main_page=1
 while True:
-	print main_page
 	file.write(str(main_page)+'\n')
 	website="https://archive.org/details/image?&sort=-downloads&page=%d"%main_page
 	website=BeautifulSoup(requests.get(website).content)
@@ -13,22 +12,27 @@ while True:
 	else:
 		for per_div in channeldiv:
 			channel_page=1
+			channel_name=per_div.find('a').get('href')
+			channel_name="https://archive.org"+channel_name
 			print per_div.find('a').find('div').string
+			print channel_name
 			file.write(str(per_div.find('a').find('div').string)+'\n')
 			while True:
-				print channel_page
 				file.write(str(channel_page)+'\n')
 				channel_link= "https://archive.org"+per_div.find('a').get('href')+"?&sort=-downloads&page=%d"%channel_page
 				channel_content= BeautifulSoup(requests.get(channel_link).content)
-				channel_img=channel_content.findAll("img",{"class":"item-img"})
+				channel_img=channel_content.findAll("div",{"class":"item-ia"})
 				if type(channel_img) is list:
 					break
 				else:
+					channel_img = channel_img[1:]
 					for img in channel_img:
-						img= img.get('source')
-						m= "https://archive.org/services/img/"+img[14:]
-						print m
-						file.write(str(m)+'\n')
-						
+						link= "https://archive.org/services/img/"+str(img.findAll('div')[4].find('a').find('img').get('source'))[14:]
+						print link
+						desc= img.find_all('div')[6].find('a').text
+						desc=desc.encode('utf-8')
+						print desc
+						file.write(str(link)+'\n')
+						file.write(desc+'\n')
 				channel_page+=1				
 	main_page+=1
