a6420b5926a72cbd7884215a49d99f7bdf9841a7
varundey <varun.silversurfer@gmail.com>
2015-07-10 17:31:20 +0530

    Fixed some minor bugs

--- total.py
+++ total.py
@@ -3,7 +3,7 @@ import requests
 file = open('total.txt','a')
 main_page=1
 while True:
-	file.write(str(main_page)+'\n')
+	print main_page
 	website="https://archive.org/details/image?&sort=-downloads&page=%d"%main_page
 	website=BeautifulSoup(requests.get(website).content)
 	channeldiv=website.findAll("div",{"class":"collection-title C C2"})
@@ -18,7 +18,7 @@ while True:
 			print channel_name
 			file.write(str(per_div.find('a').find('div').string)+'\n')
 			while True:
-				file.write(str(channel_page)+'\n')
+				print channel_page
 				channel_link= "https://archive.org"+per_div.find('a').get('href')+"?&sort=-downloads&page=%d"%channel_page
 				channel_content= BeautifulSoup(requests.get(channel_link).content)
 				channel_img=channel_content.findAll("div",{"class":"item-ia"})
