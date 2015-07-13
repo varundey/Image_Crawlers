7f7f204a9a389dbc35fa8cb3facf73a12ddf717c
varundey <varun.silversurfer@gmail.com>
2015-07-13 12:47:47 +0530

    Properly crawled archive.org and all bugs removed

--- archive.py
+++ archive.py
@@ -1,16 +1,34 @@
-from bs4 import BeautifulSoup
 import requests
-import threading
-#file = open('archive1.txt','a')
-for page in range(1,13272):
-    li=[]
-    website="https://archive.org/details/image?&sort=-downloads&page=%d"%page
-    r=requests.get(website)
-    soup=BeautifulSoup(r.content)
-    img=soup.findAll("div",{"class":"collection-title C C2"})
-    for i in img:
-        x= i.find('a').find('div').text
-     	print x
-        with open("dal.txt", "a") as f:
-    	    f.write(x)    	    
-    	f.close()
+from bs4 import BeautifulSoup as b
+main_page=1
+file=open("archive.txt","a")
+while True:
+	web="https://archive.org/details/image?&sort=-downloads&page=%d"%main_page
+	soup=b(requests.get(web).content)
+	soup=soup.findAll("div",{"class":"collection-title C C2"})
+	main_page+=1
+	for category in soup:
+		category_link=category.find('a').get('href')
+		category_name=category.find('a').find('div').text
+		file.write(str(category_name)+'\n')
+		file.write(str( "https://archive.org"+category_link)+'\n')
+		print category_name
+		category_page=1
+		while True:
+			cat="https://archive.org"+category_link+"?&sort=-downloads&page=%d"%category_page
+			cat_soup=b(requests.get(cat).content)
+			cat_soup=cat_soup.findAll('div',{"class":"ttl C C2"})
+			category_page+=1
+			cat_soup=cat_soup[1:]
+			if len(cat_soup)==0:
+				break
+			for img in cat_soup:
+				link=img.find('a').get('href')
+				desc=(img.find('a').text).encode('utf-8')
+				file.write(str(desc)+'\n')
+				file.write(str( "https://archive.org"+link)+'\n')
+			print category_page-1
+		
+	print main_page-1
+	
+	
